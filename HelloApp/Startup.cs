20bc3dd49679649e83fdf930412bd207ac995420
evgeniiivaneko <evgenii_ivanenko@live.com>
2017-09-30 16:29:28 +0300

    Работа с token

--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -11,42 +11,17 @@ namespace HelloApp
 {
     public class Startup
     {
-        IHostingEnvironment _env;
-        public Startup(IHostingEnvironment env)
-        {
-            _env = env;
-        }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            app.Map("/home", home => {
-                home.Map("/index", Index);
-                home.Map("/about", About);
-            });
-            
+            app.UseToken();
 
-            app.Run(async context => {
-                await context.Response.WriteAsync("Page is not found");
-            });
-        }
-
-        private static void Index(IApplicationBuilder app)
-        {
-            app.Run(async context => {
-                await context.Response.WriteAsync("Index");
-            });
-        }
-
-        private static void About(IApplicationBuilder app)
-        {
-            app.Run(async context => {
-                await context.Response.WriteAsync("About");
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World");
             });
         }
     }
---
+++ HelloApp/TokenMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloApp
+{
+    public class TokenMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TokenMiddleware( RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var token = context.Request.Query["token"];
+            if (String.IsNullOrWhiteSpace(token) || token != "12345678")
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Token is invalid");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
