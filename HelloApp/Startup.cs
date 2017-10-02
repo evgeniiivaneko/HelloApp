cff3d64155da7d2e3cce71236c2757d9b2b32071
evgeniiivaneko <evgenii_ivanenko@live.com>
2017-10-02 20:18:58 +0300

    Использование статических файлов

--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace HelloApp
 {
@@ -13,13 +14,23 @@ namespace HelloApp
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDirectoryBrowser();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.UseMiddleware<ErrorHandlingMiddleware>();
-            app.UseMiddleware<AuthenticationMiddleware>();
-            app.UseMiddleware<RoutingMiddleware>();
+            app.UseFileServer(new FileServerOptions
+            {
+                EnableDirectoryBrowsing = true,
+               // FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\html")),
+                RequestPath = new PathString("/pages"),
+                EnableDefaultFiles = false
+            });
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello world!");
+            });
 
         }
     }
